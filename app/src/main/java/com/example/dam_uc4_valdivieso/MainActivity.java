28c7b8911e8dde12e0faf3613384f46ddf4410b3
Jheremy val <2113110046@untels.edu.pe>
2024-12-11 16:34:37 -0500

    Segundo commit - Formulario

--- app/src/main/java/com/example/dam_uc4_valdivieso/MainActivity.java
+++ app/src/main/java/com/example/dam_uc4_valdivieso/MainActivity.java
@@ -8,17 +8,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+
+        Button btn2 = findViewById(R.id.idButtonLogin);
+        btn2.setOnClickListener(view -> Toast.makeText(this, "Primero haga un registro", Toast.LENGTH_SHORT).show());
+
+        Button btn1 = findViewById(R.id.idButtonRegistro);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Registrado", Toast.LENGTH_SHORT).show());
+
+        TextView tv2 = findViewById(R.id.idTextViewRegistrar);
+        tv2.setOnClickListener(view -> Toast.makeText(this, "Consulte con escuela", Toast.LENGTH_SHORT).show());
+
+
+
+
     }
 }
